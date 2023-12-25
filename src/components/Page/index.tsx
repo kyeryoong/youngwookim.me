43ba27d22e6b538538b04e354c4c2aa99bf45ab3
NeckyKim <kyeryoong@gmail.com>
2023-12-25 09:27:00 +0900

    feat: 페이지 컴포넌트 구현

---
+++ src/components/Page/index.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react';
+import * as S from './styled';
+
+type PageProps = {
+  children: ReactNode;
+  customStyle?: React.CSSProperties;
+};
+
+const Page = ({ children, customStyle }: PageProps) => {
+  return <S.PageWrapper style={customStyle}>{children}</S.PageWrapper>;
+};
+
+export default Page;
---
+++ src/components/Page/styled.ts
@@ -0,0 +1,8 @@
+'use client';
+import styled from 'styled-components';
+
+export const PageWrapper = styled('div')`
+  padding-left: 15vw;
+  padding-right: 15vw;
+  margin-top: 20vh;
+`;
