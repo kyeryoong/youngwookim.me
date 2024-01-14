88aa87876b248cf17fecbfbc3aa2dc3d2b751c47
NeckyKim <kyeryoong@gmail.com>
2024-01-14 19:37:37 +0900

    chore: store에 다크 모드 속성 추가

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -2,13 +2,18 @@ import { makeAutoObservable } from 'mobx';
 
 class Store {
   isMenuOpen: boolean = false;
+  isDarkMode: boolean = true;
 
   constructor() {
     makeAutoObservable(this);
   }
 
-  setIsMenuOpen = (isMenuOpen: boolean) => {
-    this.isMenuOpen = isMenuOpen;
+  setIsMenuOpen = (value: boolean) => {
+    this.isMenuOpen = value;
+  };
+
+  setIsDarkMode = (value: boolean) => {
+    this.isDarkMode = value;
   };
 }
 
