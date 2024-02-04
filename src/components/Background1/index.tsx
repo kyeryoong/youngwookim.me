22b0afd6ee83fd7153cd9ad1a9bffdd5e862994d
youngwoo_kim4 <youngwoo_kim4@tmax.co.kr>
2024-02-04 23:13:06 +0900

    chore: 홈 화면 비디오 파일 추가 및 수정

--- src/components/Background1/index.tsx
+++ src/components/Background1/index.tsx
@@ -3,15 +3,8 @@ import * as S from './styled';
 export default function Background1() {
   return (
     <S.BackgroundWrapper>
-      <S.ToggleWrapper src={'/home/toggle_wrapper.png'} alt={'Toggle Button'} width={2000} height={1000} />
-      <S.ToggleSwitch src={'/home/toggle_switch.png'} alt={'Toggle Button'} width={2000} height={1000} />
-
-      <S.Computer1 src={'/home/computer_1.png'} alt={'Computer'} width={2000} height={2000} />
-      <S.Computer2 src={'/home/computer_2.png'} alt={'Computer'} width={2000} height={2000} />
-
+      <S.Asterisk src={'/home/asterisk.mp4'} autoPlay muted loop />
       <S.ArrowBracket src={'/home/arrow_bracket.mp4'} autoPlay muted loop />
-
-      <S.Heading>Front-end</S.Heading>
     </S.BackgroundWrapper>
   );
 }
--- src/components/Background1/styled.ts
+++ src/components/Background1/styled.ts
@@ -1,6 +1,5 @@
 'use client';
 import styled from 'styled-components';
-import Image from 'next/image';
 
 export const BackgroundWrapper = styled('div')`
   width: 100vw;
@@ -39,99 +38,23 @@ export const Heading = styled('span')`
   background-clip: text;
   -webkit-background-clip: text;
   -webkit-text-stroke: 3px transparent;
+  z-index: 2;
 `;
 
-export const Graphics = styled(Image)`
+export const Asterisk = styled('video')`
+  width: 35vw;
+  height: 35vw;
   position: fixed;
-  object-fit: contain;
-  animation-name: Floating;
-  animation-duration: 4s;
-  animation-iteration-count: infinite;
-  transform: translateY(0px);
-
-  @keyframes Floating {
-    0% {
-      transform: translateY(0px);
-    }
-
-    50% {
-      transform: translateY(-50px);
-    }
-
-    100% {
-      transform: translateY(0px);
-    }
-  }
-`;
-
-export const Toggle = styled(Graphics)`
-  width: 50vw;
-  height: 50vw;
-  bottom: -7.5vw;
-  left: -15vw;
-`;
-
-export const ToggleWrapper = styled(Toggle)`
-  z-index: 100;
-`;
-
-export const ToggleSwitch = styled(Toggle)`
-  z-index: 101;
-`;
-
-export const Computer = styled(Graphics)`
-  width: 50vw;
-  height: 50vw;
-  bottom: -17.5vw;
-  right: -10vw;
-`;
-
-export const Computer1 = styled(Computer)`
-  z-index: 101;
-  animation-name: FloatingAndFading;
-  animation-duration: 4s;
-  animation-iteration-count: infinite;
-  transform: translateY(0px);
-  opacity: 0;
-
-  @keyframes FloatingAndFading {
-    0% {
-      transform: translateY(0px);
-      opacity: 0;
-    }
-
-    25% {
-      opacity: 1;
-    }
-
-    50% {
-      transform: translateY(-50px);
-      opacity: 0;
-    }
-
-    75% {
-      opacity: 1;
-    }
-
-    100% {
-      transform: translateY(0px);
-      opacity: 0;
-    }
-  }
-`;
-
-export const Computer2 = styled(Computer)`
-  z-index: 100;
+  bottom: 0vw;
+  left: -7.5vw;
+  z-index: 1;
 `;
 
 export const ArrowBracket = styled('video')`
-  width: 25vw;
-  height: 25vw;
+  width: 35vw;
+  height: 35vw;
   position: fixed;
-  top: -5vw;
-  right: 20vw;
-  animation-name: Floating;
-  animation-duration: 4s;
-  animation-iteration-count: infinite;
-  transform: translateY(0px);
+  top: -7.5vw;
+  right: -5vw;
+  z-index: 1;
 `;
