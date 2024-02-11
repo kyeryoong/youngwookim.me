ff89d784d53a0c6105e418d821777299c472a9cb
kyeryoong <kyeryoong@gmail.com>
2024-02-12 02:37:25 +0900

    feat: 메뉴창 명령어 입력 기능 구현

--- src/components/Menu/Console/index.tsx
+++ src/components/Menu/Console/index.tsx
@@ -1,26 +1,61 @@
 'use client';
 import * as S from './styled';
 import store from '@/stores/store';
+import { useRouter } from 'next/navigation';
 import { observer } from 'mobx-react';
 import React, { useState } from 'react';
 
 const Console = observer(() => {
-  const [line, setLine] = useState<string>('s');
+  const router = useRouter();
+
+  const [currentLine, setCurrentLine] = useState<string>('');
+  const [previousLines, setPreviousLines] = useState<string[]>([]);
+  const [resultLines, setResultLines] = useState<string[]>([]);
 
   const processCommand = () => {
     event?.preventDefault();
-    console.log(line);
-    setLine('');
+
+    setResultLines((prev: string[]) => [...prev, `> ${currentLine}`]);
+
+    const [prefix, suffix] = currentLine.trim().toLowerCase().split(' ');
+
+    if (prefix === 'cd') {
+      if (suffix === 'home') {
+        router.push('/');
+        store.setIsMenuOpen(false);
+      } else if (suffix === 'profile') {
+        router.push('profile');
+        store.setIsMenuOpen(false);
+      } else if (suffix === 'tech') {
+        router.push('tech');
+        store.setIsMenuOpen(false);
+      } else {
+        setResultLines((prev: string[]) => [...prev, `bash: cd: ${suffix}: No such page`]);
+      }
+    } else if (prefix === 'clear' || prefix === 'cls') {
+      setResultLines([]);
+    } else {
+      setResultLines((prev: string[]) => [...prev, `bash: ${currentLine}: command not found`]);
+    }
+
+    setPreviousLines((prev: string[]) => [...prev, currentLine]);
+    setCurrentLine('');
   };
 
   const handleLineChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
-    setLine(event.target.value);
+    setCurrentLine(event.target.value);
   };
 
   return (
     <S.ConsoleWrapper onSubmit={processCommand}>
-      <S.Line value={line} onChange={handleLineChange} />
+      <S.ResultLineWrapper>
+        {resultLines.map((resultLine: string, index: number) => (
+          <S.ResultLine key={index}>{resultLine}</S.ResultLine>
+        ))}
+      </S.ResultLineWrapper>
+      <S.CurrentLineCursor />
+      <S.CurrentLineInput value={currentLine} onChange={handleLineChange} />
     </S.ConsoleWrapper>
   );
 });
--- src/components/Menu/Console/styled.ts
+++ src/components/Menu/Console/styled.ts
@@ -3,4 +3,31 @@ import styled from 'styled-components';
 
 export const ConsoleWrapper = styled('form')``;
 
-export const Line = styled('input')``;
+export const CurrentLineCursor = styled('span')`
+  font-size: 1.2rem;
+  color: white;
+
+  &::before {
+    content: '> ';
+  }
+`;
+
+export const CurrentLineInput = styled('input')`
+  font-size: 1.2rem;
+  height: 25px;
+  line-height: 25px;
+  color: white;
+  border: none;
+  outline: none;
+  background-color: transparent;
+`;
+
+export const ResultLineWrapper = styled('div')``;
+
+export const ResultLine = styled('span')`
+  font-size: 1.2rem;
+  height: 25px;
+  line-height: 25px;
+  color: white;
+  display: block;
+`;
