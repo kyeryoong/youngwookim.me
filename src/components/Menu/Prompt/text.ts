9c146338a5aa7ce27060bb1cca28f8e19d47df77
kyeryoong <kyeryoong@gmail.com>
2024-02-14 02:32:06 +0900

    feat: fortune 명령어 실행 시, 출력할 텍스트 파일 생성

---
+++ src/components/Menu/Prompt/text.ts
@@ -0,0 +1,11 @@
+export const initText = 'youngwookim.me Terminal\n(c) Young-Woo Kim. All rights reserved.';
+
+export const helpText =
+  'cd : 페이지 이동\nls | dir : 페이지 목록\ntheme {dark | light} : 테마 변경\nclear | cls : 터미널 초기화\nhelp : 도웁말 출력\nexit: 메뉴 닫기';
+
+export const fortuneTexts = [
+  '(자연대 건물에 컴공 출신 범죄자가 숨어들었다)\n경찰: 당신 컴공이지?\n컴공: 아닌데요.\n경찰: 2!=2는 참인가 거짓인가?\n컴공: 거짓이죠.\n경찰: 끌고 가.',
+  '어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n"재귀함수가 뭔가요?"\n"잘 들어보게. 옛날에 산 꼭대기에 현자가 있었어. 질문엔 모두 지혜롭게 대답해 주었지.\n그런데 어느날, 그 선인에게 한 선비가 찾아와서 물었어.\n  "재귀함수가 뭔가요?"\n  "잘 들어보게. 옛날에 산 꼭대기..."',
+  '0과 1로만 이루어진 기차가 뭐게요~?\nbinary는 호남선~\nnum row column char에~',
+  'A: long패딩보다 긴 건 뭔지아세요?\nB: double 코트 엌ㅋㅋ\nA: 그럼 짧은건?\nB: int 패딩 엌ㅋㅋㅋㅋ',
+];
