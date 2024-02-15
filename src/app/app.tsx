4d9df48f6ee6d461a8d6c34af5a7fad440db6fe2
kyeryoong <kyeryoong@gmail.com>
2024-02-16 00:41:56 +0900

    chore: 빌드 시, CSS 스타일 오류 테스트

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -27,19 +27,19 @@ const App = observer(({ children }: AppProps) => {
     return () => window.removeEventListener('keydown', handleKeyPress);
   }, []);
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const themeMode = localStorage.getItem('youngwookim.me_theme');
+  // useEffect(() => {
+  //   if (typeof window !== 'undefined') {
+  //     const themeMode = localStorage.getItem('youngwookim.me_theme');
 
-      if (themeMode === 'dark') {
-        store.setTheme('dark');
-      } else if (themeMode === 'light') {
-        store.setTheme('light');
-      } else {
-        store.setTheme('dark');
-      }
-    }
-  }, []);
+  //     if (themeMode === 'dark') {
+  //       store.setTheme('dark');
+  //     } else if (themeMode === 'light') {
+  //       store.setTheme('light');
+  //     } else {
+  //       store.setTheme('dark');
+  //     }
+  //   }
+  // }, []);
 
   return (
     <ThemeProvider theme={store.theme === 'dark' ? dark : light}>
