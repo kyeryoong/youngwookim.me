42962a29154f27389410cc51c245fcf5ada1b92c
kyeryoong <kyeryoong@gmail.com>
2024-03-03 17:33:54 +0900

    chore: layout.tsx의 body 컴포넌트에 suppressHydrationWarning 속성 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <body>
+      <body suppressHydrationWarning>
         <StyledComponentsRegistry>
           <App>{children}</App>
         </StyledComponentsRegistry>
