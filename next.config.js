a6bb85b1e96eeb5608b2540be0befa5c730bbc25
kyeryoong <kyeryoong@gmail.com>
2024-03-03 16:49:32 +0900

    chore: next.config.js에 styled components 관련 옵션 추가

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
+/* eslint-disable no-undef */
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['avatars.githubusercontent.com'],
+  },
+  compiler: {
+    styledComponents: true,
   },
 };
 
