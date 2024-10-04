c0e0d6808d1c6085331759021f08ce6a4088e2ff
kyeryoong <kyeryoong@gmail.com>
2024-10-04 23:53:00 +0900

    chore: 홈 화면에 [page, setPage] 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,12 @@
 'use client';
 
 import { observer } from 'mobx-react-lite';
-import { useEffect, useState } from 'react';
 
 import Home from '@/components/Home';
 
 import * as S from './styled';
 
 const Main = observer(() => {
-  const [page, setPage] = useState<number>(0);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setPage((page + 1) % 3);
-    }, 5000);
-  });
-
   return (
     <S.Wrapper>
       <Home />
