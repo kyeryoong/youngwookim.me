805160b53e221ecbe5fa19d923259e533db40432
kyeryoong <kyeryoong@gmail.com>
2024-10-11 10:38:42 +0900

    chore: utils/isMobile.ts를 src 내부로 이동

--- src/components/Menu/Prompt/index.tsx
+++ src/components/Menu/Prompt/index.tsx
@@ -6,7 +6,7 @@ import { useEffect, useRef, useState } from 'react';
 
 import { useStore } from '@/stores';
 
-import { isMobile } from '../../../../utils/isMobile';
+import { isMobile } from '../../../utils/isMobile';
 import * as S from './styled';
 import { fortuneTexts, helpText, initText } from './text';
 
