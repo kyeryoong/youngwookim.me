90349be5b25f467d66c8ee5467306def296f3697
kyeryoong <kyeryoong@gmail.com>
2024-10-15 00:25:20 +0900

    chore: API의 routes에 force-dynamic 코드 추가

--- src/app/api/getPost/route.ts
+++ src/app/api/getPost/route.ts
@@ -3,6 +3,8 @@ import { NextRequest, NextResponse } from 'next/server';
 
 import { connectDB } from '../../../utils/database';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: NextRequest) {
   try {
     const database = (await connectDB).db(process.env.DATABASE_NAME);
--- src/app/api/getPosts/route.ts
+++ src/app/api/getPosts/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server';
 
 import { connectDB } from '../../../utils/database';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   try {
     const database = (await connectDB).db(process.env.DATABASE_NAME);
