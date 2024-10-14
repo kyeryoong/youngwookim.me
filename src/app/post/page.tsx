f4879c333aabc3f60b7da6a202c925068a26a948
kyeryoong <kyeryoong@gmail.com>
2024-10-14 19:30:31 +0900

    chore: 게시판 페이지에 force-dynamic 추가

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -9,6 +9,8 @@ import ReadPage from '@/components/Post/ReadPage';
 import { ScrollToTopButton } from '@/components/ScrollToTopButton';
 import { useStore } from '@/stores';
 
+export const dynamic = 'force-dynamic';
+
 const PostPage = observer(() => {
   const { postStore } = useStore();
 
