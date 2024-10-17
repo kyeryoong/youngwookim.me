6a336ca220889903c3143810869afafc1dbbc493
kyeryoong <kyeryoong@gmail.com>
2024-10-17 18:04:17 +0900

    style: 테마 모드 버튼 스타일 수정

--- src/components/Header/ThemeModeButton/styled.ts
+++ src/components/Header/ThemeModeButton/styled.ts
@@ -5,49 +5,59 @@ type ThemeModeButtonWrapperProps = {
 };
 
 export const ThemeModeButtonWrapper = styled('button')<ThemeModeButtonWrapperProps>`
-  width: 70px;
-  height: 40px;
   background-color: transparent;
   border: none;
   justify-self: end;
-  margin-right: 30px;
+  margin-right: 20px;
   transition: 0.5s;
+  outline: none;
   z-index: ${({ $show }) => ($show ? 12000 : -1)};
   pointer-events: ${({ $show }) => ($show ? 'auto' : 'none')};
   cursor: ${({ $show }) => ($show ? 'pointer' : 'default')};
   opacity: ${({ $show }) => ($show ? 1 : 0)};
 
   @media (pointer: coarse) or (max-width: 1000px) {
-    width: 56px;
-    height: 32px;
-    margin-right: 24px;
+    margin-right: min(2.5vw, 20px);
   }
 `;
 
 export const ToggleSwitchWrapper = styled('div')`
-  width: 100%;
-  height: 100%;
+  width: 64px;
+  height: 32px;
   border-radius: 40px;
-  background-color: ${({ theme }) => theme.color.gray[700]};
-  padding: 5px;
+  background-color: ${({ theme }) => theme.color.gray[800]};
+  box-shadow: 0px 0px 8px 4px rgba(50, 50, 50, 0.1);
+  position: relative;
   transition: 0.5s;
 
   @media (pointer: coarse) or (max-width: 1000px) {
-    padding: 4px;
+    width: min(8vw, 64px);
+    height: min(4vw, 32px);
   }
 `;
 
 export const ToggleSwitch = styled('div')`
-  width: 30px;
-  height: 30px;
-  border-radius: 40px;
-  background-color: ${({ theme }) => (theme.mode === 'dark' ? 'white' : 'gold')};
-  margin-left: ${({ theme }) => (theme.mode === 'dark' ? '0%' : '30px')};
+  width: 36px;
+  height: 36px;
+  border: 1px solid ${({ theme }) => theme.color.gray[500]};
+  border-radius: 32px;
+  background-color: ${({ theme }) =>
+    theme.mode === 'dark' ? 'white' : `${theme.color.yellow[600]}`};
+  box-shadow: 0px 0px 8px 4px rgba(50, 50, 50, 0.1);
+  position: absolute;
+  top: -2px;
+  left: ${({ theme }) => (theme.mode === 'dark' ? '0px' : '28px')};
   transition: 0.5s;
 
+  &:hover {
+    background-color: ${({ theme }) =>
+      theme.mode === 'dark' ? 'white' : `${theme.color.yellow[500]}`};
+  }
+
   @media (pointer: coarse) or (max-width: 1000px) {
-    width: 24px;
-    height: 24px;
-    margin-left: ${({ theme }) => (theme.mode === 'dark' ? '0%' : '24px')};
+    width: min(4.5vw, 36px);
+    height: min(4.5vw, 36px);
+    top: calc(-1 * min(0.25vw, 2px));
+    left: ${({ theme }) => (theme.mode === 'dark' ? '0px' : 'min(3.5vw, 28px)')};
   }
 `;
