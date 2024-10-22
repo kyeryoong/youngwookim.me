6bba1ec15adacb16aac6079e8b307b46bd29de82
kyeryoong <kyeryoong@gmail.com>
2024-10-23 00:40:40 +0900

    chore: appStyled.ts에 transition-property 속성 제거

--- src/app/appStyled.ts
+++ src/app/appStyled.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const AppWrapper = styled('body')`
   height: 100dvh;
   background-color: ${({ theme }) => theme.color.black};
-  transition-property: background-color;
 
   &::-webkit-scrollbar {
     width: 15px;
