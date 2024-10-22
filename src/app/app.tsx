0be3b0e15b235621de1cbdd80d3960d9d7cadeda
kyeryoong <kyeryoong@gmail.com>
2024-10-23 02:21:12 +0900

    chore: 초기 진입 시, localStorage에 있는 테마 모드 적용시키도록 수정

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,24 +1,25 @@
 'use client';
 
 import { observer } from 'mobx-react-lite';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import Menu from '@/components/Menu';
 import { useStore } from '@/stores';
+import { ThemeType } from '@/stores/themeStore';
 import dark from '@/theme/dark';
 import font from '@/theme/font';
 import light from '@/theme/light';
 import ToastPopup from '@/theme/ToastPopup';
 
-import * as S from './appStyled';
-
 type AppProps = { children: React.ReactNode };
 
 const App = observer(({ children }: AppProps) => {
   const { menuStore, themeStore } = useStore();
+
+  const [isThemeLoaded, setIsThemeLoaded] = useState<boolean>(false);
 
   useEffect(() => {
     const handleKeyPress = (event: KeyboardEvent) => {
@@ -32,15 +33,31 @@ const App = observer(({ children }: AppProps) => {
     return () => window.removeEventListener('keydown', handleKeyPress);
   }, []);
 
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const themeMode = localStorage.getItem('theme');
+
+      if (themeMode) {
+        themeStore.setTheme(themeMode as ThemeType);
+      } else {
+        localStorage.setItem('theme', 'dark');
+      }
+
+      setIsThemeLoaded(true);
+    }
+  }, []);
+
   return (
     <ThemeProvider theme={themeStore.theme === 'dark' ? { ...dark, font } : { ...light, font }}>
-      <S.AppWrapper>
-        <Header />
-        <Menu />
-        {children}
-        <Footer />
-        <ToastPopup />
-      </S.AppWrapper>
+      {isThemeLoaded && (
+        <>
+          <Header />
+          <Menu />
+          {children}
+          <Footer />
+          <ToastPopup />
+        </>
+      )}
     </ThemeProvider>
   );
 });
