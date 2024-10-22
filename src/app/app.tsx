50b59f0d7404af31cc5f7e5175763cd107777a68
kyeryoong <kyeryoong@gmail.com>
2024-10-23 00:26:34 +0900

    test: 페이지 전환 시 깜빡거리는 현상 테스트

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,13 @@
 'use client';
 
 import { observer } from 'mobx-react-lite';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import Menu from '@/components/Menu';
 import { useStore } from '@/stores';
-import { ThemeType } from '@/stores/themeStore';
 import dark from '@/theme/dark';
 import font from '@/theme/font';
 import light from '@/theme/light';
@@ -18,12 +17,8 @@ import * as S from './appStyled';
 
 type AppProps = { children: React.ReactNode };
 
-export const dynamic = 'force-dynamic';
-
 const App = observer(({ children }: AppProps) => {
   const { menuStore, themeStore } = useStore();
-
-  const [isThemeLoaded, setIsThemeLoaded] = useState<boolean>(false);
 
   useEffect(() => {
     const handleKeyPress = (event: KeyboardEvent) => {
@@ -37,31 +32,15 @@ const App = observer(({ children }: AppProps) => {
     return () => window.removeEventListener('keydown', handleKeyPress);
   }, []);
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const themeMode = localStorage.getItem('theme');
-
-      if (themeMode) {
-        themeStore.setTheme(themeMode as ThemeType);
-      } else {
-        localStorage.setItem('theme', 'dark');
-      }
-
-      setIsThemeLoaded(true);
-    }
-  }, []);
-
   return (
     <ThemeProvider theme={themeStore.theme === 'dark' ? { ...dark, font } : { ...light, font }}>
-      {isThemeLoaded && (
-        <S.AppWrapper>
-          <Header />
-          <Menu />
-          {children}
-          <Footer />
-          <ToastPopup />
-        </S.AppWrapper>
-      )}
+      <S.AppWrapper>
+        <Header />
+        <Menu />
+        {children}
+        <Footer />
+        <ToastPopup />
+      </S.AppWrapper>
     </ThemeProvider>
   );
 });
