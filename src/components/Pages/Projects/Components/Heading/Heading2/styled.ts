b32b047f119173665d6c167ff15a3861eb02ff04
kyeryoong <kyeryoong@gmail.com>
2024-11-11 17:38:20 +0900

    style: Heading2 컴포넌트의 padding-bottom 속성 제거

--- src/components/Pages/Projects/Components/Heading/Heading2/styled.ts
+++ src/components/Pages/Projects/Components/Heading/Heading2/styled.ts
@@ -4,13 +4,11 @@ export const Heading2Wrapper = styled('h2')`
   font-size: 1.2rem;
   font-weight: 400;
   width: 100%;
-  color: ${({ theme }) => theme.color.gray[600]};
-  padding-bottom: 12px;
+  color: ${({ theme }) => theme.color.gray[500]};
   margin-bottom: 12px;
 
   @media (pointer: coarse) or (max-width: 1000px) {
     font-size: min(3.6vw, 1.2rem);
-    padding-bottom: min(3vw, 12px);
     margin-bottom: min(3vw, 12px);
   }
 `;
