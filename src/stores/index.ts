6d2a571c6d5e35c458a81483cd1158a0703d3284
kyeryoong <kyeryoong@gmail.com>
2024-12-20 10:49:46 +0900

    chore: HomeStore 제거

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,18 +1,15 @@
-import { HomeStore } from './homeStore';
 import { MenuStore } from './menuStore';
 import { PostStore } from './postStore';
 import { ThemeStore } from './themeStore';
 import { UIStore } from './uiStore';
 
 export class Store {
-  homeStore: HomeStore;
   menuStore: MenuStore;
   themeStore: ThemeStore;
   postStore: PostStore;
   uiStore: UIStore;
 
   constructor() {
-    this.homeStore = new HomeStore();
     this.menuStore = new MenuStore();
     this.themeStore = new ThemeStore();
     this.postStore = new PostStore();
