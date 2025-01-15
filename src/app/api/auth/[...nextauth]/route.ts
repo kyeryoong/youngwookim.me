539d80107a86f3cc2116a34ab8d1941dc79515c9
kyeryoong <kyeryoong@gmail.com>
2025-01-16 01:25:11 +0900

    refactor: authOptions.ts의 AuthOptions를 [...nextauth]/route.ts로 이동

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,27 @@
 import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
 
-import { authOptions } from '@/lib/authOptions';
+const handler = NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || '',
+      clientSecret: process.env.GITHUB_SECRET || '',
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
+    maxAge: 1800,
+  },
+  callbacks: {
+    async signIn({ user }) {
+      if (user.email && user.email === process.env.MAIL_ID) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+});
 
-const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
