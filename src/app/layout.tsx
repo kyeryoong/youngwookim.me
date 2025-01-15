66f9af88391c10151402d242b7907f8213cffab7
kyeryoong <kyeryoong@gmail.com>
2025-01-16 01:17:07 +0900

    refactor: src/app/app.tsx의 Provider를 layout.tsx로 이동

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,14 @@ import './layout.css';
 import type { Metadata } from 'next';
 import { ReactNode } from 'react';
 
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+import Menu from '@/components/Menu';
 import pretendard from '@/font/pretendard';
-
-import App from './app';
+import AuthProvider from '@/providers/authProvider';
+import StyledComponentsProvider from '@/providers/styledComponentsProvider';
+import ThemeModeProvider from '@/providers/themeModeProvider';
+import ToastPopup from '@/theme/ToastPopup';
 
 export const metadata: Metadata = {
   title: 'youngwookim.me',
@@ -16,7 +21,17 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="ko" className={pretendard.className}>
       <body className={pretendard.className} suppressHydrationWarning>
-        <App>{children}</App>
+        <StyledComponentsProvider>
+          <ThemeModeProvider>
+            <AuthProvider>
+              <Header />
+              <Menu />
+              {children}
+              <Footer />
+              <ToastPopup />
+            </AuthProvider>
+          </ThemeModeProvider>
+        </StyledComponentsProvider>
       </body>
     </html>
   );
